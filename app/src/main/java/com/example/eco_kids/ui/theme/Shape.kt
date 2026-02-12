59f7e1b596d1b672e8c5c742498880b90fb17bb8
cervantesAxel <axelpartida584@gmail.com>
2026-02-12 11:36:34 -0600

    Aniadiendo la segunda pantalla

--- app/src/main/java/com/example/eco_kids/ui/theme/Shape.kt
+++ app/src/main/java/com/example/eco_kids/ui/theme/Shape.kt
@@ -3,16 +3,43 @@ package com.example.eco_kids.ui.theme
 import androidx.compose.foundation.shape.GenericShape
 
 val CurvedTopShape = GenericShape { size, _ ->
-    moveTo(0f, size.height)
-    lineTo(0f, size.height * 0.2f)
+    // Configuraciones de estilo
+    val topArcHeight = size.height * 0.025f //Qué tan arriba llega el domo
+    val cornerRadiusBottom = 80f //Curvatura de las esquinas inferiores
 
-    quadraticBezierTo(
-        size.width / 2,
-        0f,
-        size.width,
-        size.height * 0.2f
+
+    moveTo(0f, topArcHeight + 80f)
+
+    // LINEA TOP
+    cubicTo(
+        0f, topArcHeight,                 //Esquina izquierda
+        size.width * 0.2f, 0f,            //Subida al centro
+        size.width / 2f, 0f               //Cuspide
+    )
+    cubicTo(
+        size.width * 0.8f, 0f,            //Bajada del centro
+        size.width, topArcHeight,         //Esquina derecha
+        size.width, topArcHeight + 100f   //Final del domo
     )
 
-    lineTo(size.width, size.height)
+    // 3. LADO DERECHO
+    lineTo(size.width, size.height - cornerRadiusBottom)
+
+    // 4. ESQUINA INFERIOR DERECHA
+    quadraticBezierTo(
+        size.width, size.height,
+        size.width - cornerRadiusBottom, size.height
+    )
+
+    // 5. LÍNEA INFERIOR
+    lineTo(cornerRadiusBottom, size.height)
+
+    // 6. ESQUINA INFERIOR IZQUIERDA
+    quadraticBezierTo(
+        0f, size.height,
+        0f, size.height - cornerRadiusBottom
+    )
+
+    // Cerrar el camino
     close()
 }
--- app/src/main/java/com/example/eco_kids/view/NameScreenView.kt
+++ app/src/main/java/com/example/eco_kids/view/NameScreenView.kt
@@ -2,18 +2,26 @@ package com.example.eco_kids.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.eco_kids.R
 import com.example.eco_kids.ui.theme.CurvedTopShape
 
@@ -35,13 +43,29 @@ fun NameScreen() {
                 .fillMaxSize()
                 .padding(
                     start = 36.dp,
-                    top = 180.dp,
+                    top = 175.dp,
                     end = 36.dp,
-                    bottom = 300.dp
+                    bottom = 280.dp
                 )
                 .clip(CurvedTopShape)
                 .background(Color(0xFFFEFBE8))
                 .align(Alignment.Center)
-        )
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "¡Bienvenido!",
+                        color = Color(0xFF01586C),
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                    )
+                }
+            }
+        }
     }
 }
