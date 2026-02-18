ec7b1e25b66da144a6c151866678a6627dda67b7
Iris-Ale <irisalegro@gmail.com>
2026-02-17 21:00:58 -0600

    acabando data usuario

--- app/src/main/java/com/example/eco_kids/Screen.kt
+++ app/src/main/java/com/example/eco_kids/Screen.kt
@@ -4,4 +4,5 @@ sealed class Screen(val route: String) {
     object Splash : Screen("splash")
     object Home : Screen("home")
     object Games : Screen("games")
+    object Memorama : Screen("memorama")
 }
--- app/src/main/java/com/example/eco_kids/view/GamesScreenView.kt
+++ app/src/main/java/com/example/eco_kids/view/GamesScreenView.kt
@@ -1,10 +1,13 @@
 package com.example.eco_kids.view
 
+import android.R.attr.enabled
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,90 +16,157 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.remote.creation.modifiers.RoundedRectShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.example.eco_kids.R
+import com.example.eco_kids.Screen
 import com.example.eco_kids.viewmodel.UserViewModel
 
 @Composable
 fun GamesScreen (
+    navController: NavController,
     userViewModel: UserViewModel
-){
+) {
     val name by userViewModel.userName.collectAsState(initial = "")
     val pet by userViewModel.userPet.collectAsState(initial = -1)
 
 
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize()
+            .background(color = Color(0xFFEAE2C7)),
         contentAlignment = Alignment.Center
+
     ) {
         Image(
-            painter = painterResource(id = R.drawable.ic_fondo_inicio),
-            contentDescription = null,
+            painter = painterResource(R.drawable.ic_fondo_inicio),
+            contentDescription = "fondo",
             modifier = Modifier.fillMaxSize(),
             contentScale = androidx.compose.ui.layout.ContentScale.Crop
         )
 
-        Column(modifier = Modifier.fillMaxSize()) {
-            Text(
-                text = "Hola, $name",
-                fontSize = 22.sp
-            )
+        //aqui va header
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(top = 30.dp)
+        ) {
 
-            val mascotas = listOf(
-                R.drawable.mascota_1,
-                R.drawable.mascota_2,
-                R.drawable.mascota_3,
-                R.drawable.mascota_4,
-                R.drawable.mascota_5,
-                R.drawable.mascota_6,
-                R.drawable.mascota_7,
-                R.drawable.mascota_8,
-                R.drawable.mascota_9
-            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp)
+                    .drawBehind {
 
-            val selectedPet = mascotas.getOrNull(pet)
+                        val width = size.width
+                        val height = size.height
 
-            selectedPet?.let {
-                Image(
-                    painter = painterResource(id = it),
-                    contentDescription = null,
-                    modifier = Modifier.size(120.dp)
+                        val path = Path().apply {
+
+                            // Curva superior
+                            moveTo(0f, height * 0.15f)
+
+                            quadraticBezierTo(
+                                width / 2,
+                                height * 0.35f,
+                                width,
+                                height * 0.15f
+                            )
+
+                            lineTo(width, height * 0.8f)
+
+                            quadraticBezierTo(
+                                width / 2,
+                                height * 1.05f,
+                                0f,
+                                height * 0.8f
+                            )
+
+                            close()
+                        }
+
+                        drawPath(
+                            path = path,
+                            color = Color(0xFF2E7D32)
+                        )
+                    }
+                    .padding(horizontal = 22.dp),
+                contentAlignment = Alignment.CenterStart
+            ) {
+
+                val mascotas = listOf(
+                    R.drawable.mascota_1,
+                    R.drawable.mascota_2,
+                    R.drawable.mascota_3,
+                    R.drawable.mascota_4,
+                    R.drawable.mascota_5,
+                    R.drawable.mascota_6,
+                    R.drawable.mascota_7,
+                    R.drawable.mascota_8,
+                    R.drawable.mascota_9
                 )
+
+                Row(verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start) {
+                    val selectedPet = mascotas.getOrNull(pet)
+
+                    selectedPet?.let {
+                        Image(
+                            painter = painterResource(id = it),
+                            contentDescription = null,
+                            modifier = Modifier.size(70.dp)
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.size(5.dp))
+
+                    Text(
+                        text = "Hola, $name!",
+                        color = Color.White,
+                        fontSize = 22.sp
+                    )
+
+                    Spacer(modifier = Modifier.height(40.dp))
+                }
             }
-
-            Box( modifier = Modifier.fillMaxWidth()
-                .padding(16.dp)
-                .background(Color.White),
-                contentAlignment = Alignment.Center){
-
-                //pendiente de poner esta barra
-            }
-
         }
-        Column (modifier = Modifier.fillMaxSize(),
+
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally){
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-            GameCard(R.drawable.ic_logo_inicio)
+            GameCard(R.drawable.memorama_banner, "memorama") {
+                navController.navigate(Screen.Memorama.route)
+            }
             Spacer(modifier = Modifier.height(20.dp))
-            GameCard(R.drawable.ic_logo_inicio)
+            GameCard(R.drawable.memorama_banner, "atrapar") {
+                navController.navigate(Screen.Memorama.route)
+            }
             Spacer(modifier = Modifier.height(20.dp))
-            GameCard(R.drawable.ic_logo_inicio)
-
+            GameCard(R.drawable.memorama_banner, "tercerjuego") {
+                navController.navigate(Screen.Memorama.route)
+            }
         }
     }
 }
 
+
 @Composable
-fun GameCard (imagen: Int){
+fun GameCard (imagen: Int, desc: String, onClick: () -> Unit){
 
     Box(
         modifier = Modifier.fillMaxWidth(0.8f)
@@ -105,15 +175,17 @@ fun GameCard (imagen: Int){
             .background(
                 shape = RoundedCornerShape(18.dp),
                 color = Color.White
-            ),
+            )
+            .clickable { onClick() },
         contentAlignment = Alignment.Center
     ){
         Image(
-            painter = painterResource(id = R.drawable.ic_fondo_inicio),
-            contentDescription = "memorama",
+            painter = painterResource(id = imagen),
+            contentDescription = desc,
             modifier = Modifier.fillMaxSize(),
             contentScale = androidx.compose.ui.layout.ContentScale.Crop
         )
 
     }
 }
+
